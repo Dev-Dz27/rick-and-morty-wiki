39dd4612fa357e4490111e2a6a2c70ebfb7b9cd6
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2022-09-05 23:27:23 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
   },
   plugins: [],
 }
