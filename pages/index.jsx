f83ccf55e75044ae63f999b347d6e72dd747b648
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2022-09-17 21:04:51 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -87,9 +87,10 @@ const Home = ({ data }) => {
   return (
     <div className=" min-h-screen flex-col items-center justify-center py-2">
       <Head>
+        <meta name="google-site-verification" content="zNwtizC2UvJlyJIAGT_MJpvvwk1uXh9cZxUAGqSrXgU" />
         <title>Wubba Lubba Dub dub!</title>
         <link rel="icon" href="/favicon.ico" />
-    <meta name="google-site-verification" content="zNwtizC2UvJlyJIAGT_MJpvvwk1uXh9cZxUAGqSrXgU" />
+
       </Head>
 
       <button
